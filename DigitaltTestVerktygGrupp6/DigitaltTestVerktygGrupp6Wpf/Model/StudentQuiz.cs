5bed36d08b7968eeaacca4810a96521acc6b8741
Örjan Carlsson <orjancarlsson90@gmail.com>
2016-04-28 09:40:42 +0200

    Made changes to the database

---
+++ DigitaltTestVerktygGrupp6/DigitaltTestVerktygGrupp6Wpf/Model/StudentQuiz.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DigitaltTestVerktygGrupp6Wpf.Model
+{
+    public class StudentQuiz
+    {
+        [Key, Column(Order = 0)]
+        public int StudentId { get; set; }
+        [Key, Column(Order = 1)]
+        public int QuizId { get; set; }
+
+        public virtual Student Students { get; set; }
+        public virtual Quiz Quizes { get; set; }
+
+        public int Time { get; set; }
+        public string Points { get; set; }
+    }
+}
