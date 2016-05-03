7b69029df416ae266d58a00e74032dda4926ebf5
charlarn <cbfauxpas@gmail.com>
2016-05-03 21:59:45 +0200

    added default questions

--- DigitaltTestVerktygGrupp6/DigitaltTestVerktygGrupp6Wpf/Database/dbDataContext.cs
+++ DigitaltTestVerktygGrupp6/DigitaltTestVerktygGrupp6Wpf/Database/dbDataContext.cs
@@ -9,7 +9,7 @@ namespace DigitaltTestVerktygGrupp6Wpf.Database
 {
     public class dbDataContext : DbContext
     {
-        
+
         public DbSet<dbAlternative> Alternatives { get; set; }
         public DbSet<dbQuestion> Questions { get; set; }
         public DbSet<dbQuiz> Quizes { get; set; }
@@ -17,7 +17,7 @@ namespace DigitaltTestVerktygGrupp6Wpf.Database
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
-            modelBuilder.Conventions.Remove<System.Data.Entity.ModelConfiguration.Conventions.PluralizingTableNameConvention>();
+            // modelBuilder.Conventions.Remove<System.Data.Entity.ModelConfiguration.Conventions.PluralizingTableNameConvention>();
 
             // Add any configuration or mapping stuff here
         }
@@ -26,14 +26,71 @@ namespace DigitaltTestVerktygGrupp6Wpf.Database
         {
 #if DEBUG
             // Create my debug (testing) objects here
-           
+
 #endif
 
             // Normal seeding goes here
             IList<dbStudent> defaultStudent = new List<dbStudent>();
             IList<dbQuiz> defaultQuiz = new List<dbQuiz>();
+            IList<dbQuestion> defaultQuestions = new List<dbQuestion>();
+            List<dbAlternative> defaultAlternatvies = new List<dbAlternative>();
 
-            defaultStudent.Add(new dbStudent {
+            defaultQuestions.Add(new dbQuestion
+            {
+                Text = "Välj det största talet",
+                Type = QuestionType.Single.ToString(),
+                Points = 10,
+                Alternatives = new List<dbAlternative> { new dbAlternative{ Text = "2", IsCorrect = 0 }, new dbAlternative { Text = "3", IsCorrect = 0 },
+                new dbAlternative { Text = "7", IsCorrect = 1 } }
+            });
+            defaultQuestions.Add(new dbQuestion
+            {
+                Text = "Vad är 2 + 2?",
+                Type = QuestionType.Text.ToString(),
+                Points = 5,
+                Alternatives = new List<dbAlternative> { new dbAlternative { Text = "4", IsCorrect = 1 } }
+            });
+            defaultQuestions.Add(new dbQuestion
+            {
+                Text = "Välj de tal som är udda",
+                Type = QuestionType.Multi.ToString(),
+                Points = 15,
+                Alternatives = new List<dbAlternative> { new dbAlternative{ Text = "3", IsCorrect = 1 }, new dbAlternative { Text = "4", IsCorrect = 0 },
+                new dbAlternative { Text = "7", IsCorrect = 1 }, new dbAlternative {Text="8", IsCorrect=0 }
+            }
+            });
+            defaultQuestions.Add(new dbQuestion
+            {
+                Text = "Sortera från störst till minst",
+                Type = QuestionType.Rank.ToString(),
+                Points = 20,
+                Alternatives = new List<dbAlternative> { new dbAlternative{ Text = "11", IsCorrect = 3 }, new dbAlternative { Text = "13", IsCorrect = 2 },
+                new dbAlternative { Text = "18", IsCorrect = 1 }, new dbAlternative {Text="24", IsCorrect=0 }
+            }
+            });
+
+            defaultQuiz.Add(new dbQuiz
+            {
+                dbQuizId = 1,
+                Name = "Matte",
+                Intro = "Compare numbers",
+                GradeG = 50,
+                GradeVG = 70,
+                TimeLimit = 100,
+                Questions = defaultQuestions
+            });
+            defaultQuiz.Add(new dbQuiz
+            {
+                dbQuizId = 2,
+                Name = "Quiz2",
+                Intro = "Stuff123",
+                GradeG = 55,
+                GradeVG = 75,
+                TimeLimit = 120
+            });
+
+            defaultStudent.Add(new dbStudent
+            {
                 dbStudentId = 1,
                 FirstName = "Ottar",
                 LastName = "Kallesson",
@@ -50,24 +107,7 @@ namespace DigitaltTestVerktygGrupp6Wpf.Database
                 Password = "123",
                 Email = "OrjanCool92@stuff.se"
             });
-            defaultQuiz.Add(new dbQuiz
-            {
-                dbQuizId = 1,
-                Name = "Quiz1",
-                Intro = "Stuff123",
-                GradeG = 50,
-                GradeVG = 70,
-                TimeLimit = 100
-            });
-            defaultQuiz.Add(new dbQuiz
-            {
-                dbQuizId = 2,
-                Name = "Quiz2",
-                Intro = "Stuff123",
-                GradeG = 55,
-                GradeVG = 75,
-                TimeLimit = 120
-            });
+            
             foreach (dbStudent stu in defaultStudent)
                 Context.Students.Add(stu);
             foreach (dbQuiz quiz in defaultQuiz)
@@ -98,7 +138,7 @@ namespace DigitaltTestVerktygGrupp6Wpf.Database
         static dbDataContext()
         {
 #if DEBUG
-           System.Data.Entity.Database.SetInitializer<dbDataContext>(new DropCreateIfChangeInitializer());
+            System.Data.Entity.Database.SetInitializer<dbDataContext>(new DropCreateIfChangeInitializer());
 
 #else
         Database.SetInitializer<MyDbContext> (new CreateInitializer ());
@@ -107,5 +147,5 @@ namespace DigitaltTestVerktygGrupp6Wpf.Database
 
 
     }
-   
+
 }
